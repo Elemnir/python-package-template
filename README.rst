f475857284cac78c69ad086abc8a4945494f3456
Adam Howard <ahoward0920@gmail.com>
2020-11-13 18:31:41 -0500

    Initial Commit

---
+++ README.rst
@@ -0,0 +1,17 @@
+=========
+ Example
+=========
+
+Example description
+
+--------------
+ Installation
+--------------
+
+``example`` can be installed from PyPI, and requires no external dependencies.
+
+::
+    
+    ?> pip install example
+
+
---
+++ src/example/__init__.py
@@ -0,0 +1,12 @@
+__version__     = "0.0.0"
+
+__title__       = "example"
+__description__ = "Example description"
+__uri__         = "https://github.com/Elemnir/example"
+
+__author__      = "Adam Howard"
+__email__       = "ahoward@utk.edu"
+
+__license__     = "BSD 3-clause"
+__copyright__   = "Copyright (c) Adam Howard"
+
